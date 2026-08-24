09bf8eceb27b8ae3be351792012c0b742a369c51
jeejzinha <jessicath2018@gmail.com>
2026-08-24 20:03:14 -0300

    Update Aula1.java

--- Aula1.java
+++ Aula1.java
@@ -1,5 +1,21 @@
 /**************************************************************
                    Aula 01 Java 17/08/2026
+                         Exercicio 01
+                    Printe um "ola mundo" 
+                       feito pela jeej                             
+***************************************************************/
+
+public class ex01 {
+  
+    public static void main(String[] args) {
+       System.out.println("ola mundo");
+    }
+    
+}
+
+
+/**************************************************************
+                   Aula 01 Java 17/08/2026
                          Exercicio 02
                 Pergunte o nome da pessoa e dê ola pra ela 
                        feito pela jeej                             
@@ -50,11 +66,60 @@ public class ex3{
 /**************************************************************
                    Aula 01 Java 17/08/2026
                          Exercicio 04
-                Pegue 2 notas do usuario e faca a media dele 
+       Faça um algoritmo q converte celcius em fahrenheit 
                        feito pela jeej                             
 ***************************************************************/
 
+import java.util.Scanner;
+public class Ex04 {
+    public static void main(String[]args){
+    Scanner entrada = new Scanner(System.in);
 
+    System.out.print("temperatura em graus celcius:");
+    double graus = entrada.nextDouble();
+
+    double F = 1.8 * graus + 32;
+
+
+   System.out.printf("resposta: %.2f", F);
+    entrada.close();
+    
+}
+}
+
+/**************************************************************
+                   Aula 01 Java 17/08/2026
+                         Exercicio 05
+               Reajuste o salario do usuario 
+                       feito pela jeej                             
+***************************************************************/
+
+//salario menor q 1500, reajuste de 7%; salario maior
+//ou igual a 1500, reajuste de 5.25%
+
+import java.util.Scanner;
+public class Ex05 {
+
+    public static void main(String[]args){
+    Scanner entrada = new Scanner(System.in);
+
+    System.out.print("seu salario:");
+    double sal = entrada.nextDouble();
+    double nsal; 
+   if (sal < 1500) {
+        nsal = sal + sal * 0.07;
+
+   }
+    else  {
+        nsal = sal + sal * 0.0525;
+    }
+
+     System.out.printf("seu novo salario: %.2f", nsal);
+    entrada.close();
+    
+    
+}
+}
 
 
 
